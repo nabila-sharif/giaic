19aedb0da4de8f3d6db63cfbf5d2dd1632bad30a
nabila sharif <161031798+nabila-sharif@users.noreply.github.com>
2024-10-10 19:29:52 +0500

    next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,124 @@
+import React from "react";
 import Image from "next/image";
 
-export default function Home() {
+export default function IDCard() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="flex flex-col min-h-screen bg-white">
+      <div className="flex-grow flex items-center justify-center p-6">
+        <div className="bg-slate-200 p-12 rounded-lg max-w-4xl w-full flex items-center justify-center border-4 border-sky-300">
+          {/*Background*/}
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="relative w-full h-full max-w-[350px] max-h-[400px]">
+              <Image
+                src="/images/background.jpg"
+                alt="background image blur"
+                width={350}
+                height={400}
+                objectFit="cover"
+                className="opacity-10 object-cover h-full w-full"
+              />
+            </div>
+          </div>
+
+          {/*Left Section*/}
+
+          <div className="w-2/3 pr-4 pt-16 relative z-10">
+            {/*logo*/}
+            <img
+              src="/images/download.png"
+              alt="Card logo"
+              className="absolute top-[-18px] left-0 w-16 h-18 z-10"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                {""}
+                <strong>Name: </strong>
+              </span>
+              <span className="text-black">Nabila Sharif</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                {""}
+                <strong>Roll No: </strong>
+              </span>
+              <span className="text-black">00478929</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                {""}
+                <strong>Distance learning: </strong>
+              </span>
+              <span className="text-black">No</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                <strong>City: </strong>
+              </span>
+              <span className="text-black">Karachi</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                <strong>Center: </strong>
+              </span>
+              <span className="text-black">Governor House Karachi</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                <strong>Campus: </strong>
+              </span>
+              <span className="text-black">Main</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                <strong>Days/Time: </strong>
+              </span>
+              <span className="text-black">Monday-02:00 PM - 05:00 PM</span>
+            </p>
+
+            <p className="mb-2">
+              <span className="text-sky-500">
+                <strong>Batch: </strong>
+              </span>
+              <span className="text-black">1</span>
+            </p>
+
+            {/*Button Section*/}
+            <div className="flex flex-col mt-4">
+              <button className="relative w-full p-1 rounded-lg border border-grey-300 bg-blue-900 text-white items-center justify-center flex">
+                <span
+                  className="absolute inset-0 bg-slate-500"
+                  style={{ width: "50%" }}
+                ></span>
+
+                <span className="relative z-10">Q2 & WMD</span>
+              </button>
+            </div>
+          </div>
+
+          {/*Right Section*/}
+          <div className="w-1/3 text-center relative z-10">
+            <Image
+              src={"/images/image.jpeg"}
+              alt={"profile picture"}
+              width={750}
+              height={800}
+              className="rounded-lg mb-6 w-full border border-blue-600"
+            />
+
+            <p className="border-t-2 border-t-slate-400 pt-2 font-semibold text-sky-500 mt-10 text-2xl">
+              Authorized Signature
+            </p>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
